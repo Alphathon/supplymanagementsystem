3bf3f690cc4990a945928e1f947f6aa989dc13cd
balaji <balajicherlopalli555@gmail.com>
2019-08-10 14:12:11 +0530

    added

--- src/main/java/com/supplymanagementsystem/SupplymanagementsystemApplication.java
+++ src/main/java/com/supplymanagementsystem/SupplymanagementsystemApplication.java
@@ -1,9 +1,11 @@
 package com.supplymanagementsystem;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SupplymanagementsystemApplication {
 
 	public static void main(String[] args) {
