69e5f70dacd80f0f2908563cf6740c9b04c0c23b
balaji <balajicherlopalli555@gmail.com>
2019-08-10 12:45:15 +0530

    added project

---
+++ src/main/java/com/supplymanagementsystem/SupplymanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.supplymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplymanagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplymanagementsystemApplication.class, args);
+	}
+
+}
